22a2807cddb91505cc4029cbf055465fd85d5b98
Vlad <vlad09fedko@gmaul.com>
2026-04-21 22:43:53 +0300

    Fix a bug in limits

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 // first
 console.log('~~~~~ First task ~~~~~');
 function createLogger(prefix) {
-  return function strLogger(message) {
+  return function (message) {
     return `${prefix}: ${message}`;
   };
 }
@@ -19,6 +19,7 @@ console.log('~~~~~ Second task ~~~~~');
 function createLimiter(limit) {
   let count = 0;
   return function () {
+    if (limit < 0) return 'Error!';
     if (typeof limit !== 'number' || Number.isNaN(limit)) return 'Type error';
     if (count === limit) return 'Error';
     count++;
